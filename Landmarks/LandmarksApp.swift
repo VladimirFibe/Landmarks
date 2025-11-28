7c1177738725b1ece278981a729d2e92e54366c1
Vladimir Fibe <motiw@me.com>
2025-11-28 22:00:48 +0500

    start

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -2,24 +2,16 @@
 //  LandmarksApp.swift
 //  Landmarks
 //
-//  Created by MacService on 26.04.2022.
+//  Created by Vladimir Fibe on 28.11.2025.
 //
 
 import SwiftUI
 
 @main
 struct LandmarksApp: App {
-  @StateObject private var modelData = ModelData()
-  let persistenceController = PersistenceController.shared
-  
-  var body: some Scene {
-    WindowGroup {
-      LandmarkList()
-        .environmentObject(modelData)
-      
-      
-      //            ContentView()
-      //                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
     }
-  }
 }
